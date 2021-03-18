26befa4a2d54a4c53d664ad1195398000e47ac65
carlacou <65657145+carlacou@users.noreply.github.com>
2021-03-18 00:39:02 -0300

    Add files via upload

---
+++ js/file.js
@@ -0,0 +1,20 @@
+let keys = document.querySelectorAll('.keys');
+let result = document.querySelector('#result');
+let userInput = document.querySelector('#userInput');
+
+keys.forEach(items => {
+    items.addEventListener('click', () => {
+        if(items.innerText != '='){
+            userInput.innerText += items.innerText;
+        }
+        if(items.innerText == 'C'){
+            userInput.innerText = '';
+            result.innerText = '';
+        }
+        if(items.innerText == '=' && userInput.innerText != ''){
+            result.innerText = eval(userInput.innerText);
+            userInput.innerText = '';
+        }
+    })
+})
+
